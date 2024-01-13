575157afb2a08b5175505be5eea130e2b23e8c93
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2024-01-12 22:52:12 -0500

    feat: update favicon; responsive styles; remove alpine.js

--- assets/js/data.js
+++
@@ -1,6 +0,0 @@
-import data from '../json/deck-one.json' assert { type: 'json' };
-
-// fetch('../json/deck-one.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.log(error));
