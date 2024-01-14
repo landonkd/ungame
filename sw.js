4d3adcf34591ddc9d657c6eff6fdcae3896b5d0b
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2024-01-14 00:18:38 -0500

    fix: debugging service worker

--- sw.js
+++ sw.js
@@ -139,6 +139,11 @@ self.addEventListener('fetch', function (event) {
 	// CSS & JavaScript
 	// Offline-first
 	if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript') || request.headers.get('Accept').includes('application/javascript')) {
+
+		console.log(request);
+		console.log(caches);
+		console.log('===============');
+		
 		event.respondWith(
 			caches.match(request).then(function (response) {
 				return response || fetch(request).then(function (response) {
